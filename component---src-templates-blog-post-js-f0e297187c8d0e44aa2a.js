0b614d64ceec06d8d7e00fadd49843d5b0a93127
franciscoamado <franciscoamado@users.noreply.github.com>
2025-02-01 16:13:11 +0000

    deployed via Gatsby Publish Action 🎩 for 8c7e89ab6174c3e16a1b011fda46137ce12a73d8

---
+++ component---src-templates-blog-post-js-f0e297187c8d0e44aa2a.js
@@ -0,0 +1,32 @@
+"use strict";(self.webpackChunkfranciscoamado_github_io=self.webpackChunkfranciscoamado_github_io||[]).push([[751],{6093:function(t,e,n){n.r(e);var r=n(7387),o=n(6540),a=n(4794),i=n(7581),l=n(8492),c=n(2269),s=n(5765);let p=function(t){function e(){return t.apply(this,arguments)||this}return(0,r.A)(e,t),e.prototype.render=function(){const t=this.props.data.markdownRemark,e=this.props.data.site.siteMetadata.title,{previous:n,next:r}=this.props.pageContext;return o.createElement(l.A,{location:this.props.location,title:e},o.createElement(c.A,{title:t.frontmatter.title,description:t.frontmatter.description||t.excerpt}),o.createElement(d,null,o.createElement("h1",null,t.frontmatter.title),o.createElement("p",{style:{...(0,s.hs)(-.2),display:"block",marginBottom:(0,s.di)(1),marginTop:(0,s.di)(-1)}},t.frontmatter.date),o.createElement("div",{dangerouslySetInnerHTML:{__html:t.html}}),o.createElement("hr",{style:{marginBottom:(0,s.di)(1)}}),o.createElement("h4",null,"That's it for now 👋🏻")),o.createElement("ul",{style:{display:"flex",flexWrap:"wrap",justifyContent:"space-between",listStyle:"none",padding:0}},o.createElement("li",null,r&&o.createElement(m,{to:`${r.fields.slug}`,rel:"next"},"← ",r.frontmatter.title)),o.createElement("li",null,n&&o.createElement(m,{to:`${n.fields.slug}`,rel:"prev"},n.frontmatter.title," →"))))},e}(o.Component);e.default=p;const d=i.Ay.div`
+  position: relative;
+  line-height: 1.6em;
+  padding: 0 120px 6vw;
+  min-height: 230px;
+`,m=(0,i.Ay)(a.Link)`
+  color: #323232;
+  background: transparent;
+  background-color: transparent;
+  font-weight: bolder;
+  text-decoration: none !important;
+
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    color: #323232;
+    background: transparent;
+    background-color: transparent;
+    text-decoration: none !important;
+  }
+
+  &:hover {
+    color: #323232;
+    background: transparent;
+    background-color: transparent;
+    text-decoration: none !important;
+    opacity: 0.8;
+  }
+`}}]);
+//# sourceMappingURL=component---src-templates-blog-post-js-f0e297187c8d0e44aa2a.js.map
